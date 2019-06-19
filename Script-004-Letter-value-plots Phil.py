c533a1104d464887395710f03c5ac636561b5245
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-06-19 19:41:09 +0800

    Add files via upload

--- Script-004-Letter-value-plots Phil.py
+++ Script-004-Letter-value-plots Phil.py
@@ -1,22 +1,26 @@
-#!/usr/bin/env python
-# coding: utf-8
-#
+import os
+import pandas as pd
 import seaborn as sb
 from matplotlib import pyplot as plt
-import pandas as pd
-import os
-#
-os.chdir('/Users/pauline/Documents/Python')
-dfM = pd.read_csv("Tab-PhilBathy.csv")
-dfM.head(5)
-#
+
 sb.set_style('darkgrid')
-sb.catplot(data=dfM, kind="boxen",palette='tab20c', scale = "exponential",
-           outlier_prop = 0.007, orient="v", legend=True, legend_out=True, margin_titles=True)
+sb.set_context("paper")
+
+os.chdir('/Users/pauline/Documents/Python')
+dfM = pd.read_csv("Tab-Bathy.csv")
+dfM.head(5)
+
+# plotting
+sb.catplot(
+           data=dfM, kind="boxen", palette='Paired',
+           orient="v", legend=True, legend_out=True, margin_titles=True
+           )
 plt.xticks(rotation=45)
-plt.ylabel('Elevation, m')
-plt.title('Letter-value plot for the topography of the 25 profiles cross secting \nPhilippine Trench and the Philippine archipelago', fontsize=12, fontfamily='serif')
-txt="Graphics: Python, Matplotlib & Seaborn libraries"
-plt.figtext(0.80, 0.005, txt, wrap=True, horizontalalignment='center', fontsize=10)
+plt.title('Letter-value plots for the Mariana Trench bathymetry',
+          fontsize=12, fontfamily='serif')
+
+# visualization and printing
+plt.tight_layout()
+plt.savefig('plot_Letter-value.png', dpi=300)
 plt.subplots_adjust(bottom=0.15,top=0.85)
 plt.show()
